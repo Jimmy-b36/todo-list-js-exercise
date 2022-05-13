fd71d03edf68a1c92c90fcc97a64bdb610230ab4
James Ball <jamie.j.ball@gmail.com>
2022-05-13 11:53:23 -0700

    refactored to be OO

--- index.js
+++ index.js
@@ -2,31 +2,29 @@
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title, description = "No description") {
-  const task = {
-    title,
-    complete: false,
-    description,
-
-    logState: function () {
-      console.log(
-        `${this.title} has${this.complete ? " " : " not "}been completed`
-      );
-    },
-    completeTask: function () {
-      this.complete = true;
-    },
-  };
-  return task;
+class Task {
+  constructor(title, description = "No description entered") {
+    this.title = title;
+    this.complete = false;
+    this.description = description;
+  }
+  logState() {
+    console.log(
+      `${this.title} has${this.complete ? " " : " not "}been completed`
+    );
+  }
+  completeTask() {
+    this.complete = true;
+  }
 }
 
-const task1 = newTask(
+const task1 = new Task(
   "Clean Cat Litter",
   "Take all the 💩 out of the litter box"
 );
 // task 0
-const task2 = newTask("Do Laundry", "😨"); // task 1
-
+const task2 = new Task("Do Laundry", "😨"); // task 1
+console.log(task1);
 task1.logState(); //Clean Cat Litter has not been completed
 task1.completeTask("Clean Cat Litter");
 task1.logState();
